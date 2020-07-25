230c2939d1955399d05f8610019ee9914871d040
Jordan Long <jblong27@gmail.com>
2020-07-25 13:37:07 -0500

    pushing final draft

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uuidv1 = require('uuid/v1');
+const uuid = require('uuid');
 
 const app =  express();
 
@@ -22,12 +22,12 @@ app.get("api/notes", (req, res) => {
 // post request
 app.post("/api/notes", (req, res) => {
     fs.readFile("db/db.json", (err, data) => {
-        if(err) throw err;
+        if (err) throw err;
         let json = JSON.parse(data);
         let note = {
             title: req.body.title,
             text: req.body.text,
-            id: uuidv1()
+            id: uuid()
         }
         json.push(note);
         fs.writeFile("db/db.json", JSON.stringify(json, null, 2), (err) => {
@@ -36,3 +36,35 @@ app.post("/api/notes", (req, res) => {
         });
     });
 });
+
+// delete request
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("db/db.json", (err, data) => {
+        if (err) throw err;
+        let deleteId = req.params.id;
+        let json = JSON.parse(data);
+        json.forEach((item, i) => {
+            if (item.id.includes(deleteId)) {
+                json.splice(i, 1);
+            }
+        });
+        fs.writeFile("db/db.json", JSON.stringify(json, null, 2), (err) => {
+            if (err) throw err; 
+            res.send("200");
+        });
+    });
+});
+
+// routing
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
