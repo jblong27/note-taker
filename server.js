437247118fdab708f879758156e009fb35b2bb51
Jordan Long <jblong27@gmail.com>
2020-07-25 15:02:28 -0500

    more debugging

--- server.js
+++ server.js
@@ -56,13 +56,16 @@ app.delete("/api/notes/:id", (req, res) => {
 });
 
 // routing
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-app.use(express.static(path.join(__dirname, "public")));
+  
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
+  
+app.use(express.static(path.join(__dirname, 'public')));
+
 //port and listener
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, function() {
